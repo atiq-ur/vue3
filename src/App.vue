7e9474282a618353bda9c3f394aa64078924d071
Atiqur Rahman <ar.diu.cse@gmail.com>
2021-06-14 01:03:07 +0600

    Markdown app completed

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
 <AppHeader />
   <div class="w-full flex">
-      <!-- <DcHeros /> -->
-      <Calendar />
+     <router-view></router-view>
   </div>
 </template>
 
 <script>
 import AppHeader from "./components/AppHeader";
-//import DcHeros from "./components/DcHeros";
-import Calendar from "./components/Calendar";
+
 export default {
-  components: {AppHeader,Calendar},
+  components: {AppHeader},
 }
 </script>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,12 +3,24 @@
     class="w-full bg-gradient-to-r 
     from-blue-800 to-blue-600 
     text-white px-4 py-3">
-    <a>Dc Heros</a>
+    <router-link v-for="item in list" :key="item.to" 
+    class="mx-2" :to="item.to">{{ item.title }}</router-link>
+
 </nav>
 </template>
 
 <script>
 export default {
+    data(){
+        return {
+            list:[
+                { title: "Home", to: "/" },
+                { title: "Dc Heros", to: "/dc-heros" },
+                { title: "Calendar", to: "/calendar" },
+                { title: "Markdown", to: "/markdown" },
+            ]
+        }
+    }
 
 }
 </script>
